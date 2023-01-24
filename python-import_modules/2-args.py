c5e4b5c0a864a129394f6125d671108d73e9b464
mxnctblt <thibaultmaxence44@gmail.com>
2023-01-24 06:23:03 -0800

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv) - 1):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
