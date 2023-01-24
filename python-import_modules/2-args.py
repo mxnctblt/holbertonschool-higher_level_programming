2e55a4e82f36f84c2d0feb83e1b9cd5fab56033c
mxnctblt <thibaultmaxence44@gmail.com>
2023-01-24 06:29:13 -0800

    fix argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,11 @@
 if __name__ == "__main__":
     import sys
 
-    print("{} arguments:".format(len(sys.argv) - 1))
+    if len(sys.argv) - 1 == 0:
+        print("0 arguments.")
+    elif len(sys.argv) - 1 == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(len(sys.argv) - 1):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
