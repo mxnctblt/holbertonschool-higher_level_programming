c36661b31e1448bbf0b8e95c702c15be668dbb6e
mxnctblt <thibaultmaxence44@gmail.com>
2023-01-25 06:54:49 -0800

    make it shorter

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,5 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    n = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > n:
-            n = my_list[i]
-    return n
+    my_list.sort()
+    return (my_list.pop())
