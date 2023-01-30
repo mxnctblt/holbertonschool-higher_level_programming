fb62fa529c15a7359693890e73e77704d7bb4c13
mxnctblt <thibaultmaxence44@gmail.com>
2023-01-30 06:57:57 -0800

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Defines a class Square"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size):
+        """initializes a square
+
+        Args:
+            size (int): the size of the square.
+        """
+        self.__size = size
