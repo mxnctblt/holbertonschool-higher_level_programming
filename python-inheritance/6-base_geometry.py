d2a749a3a417dc3eeb8051fceec5db8503b0d405
mxnctblt <thibaultmaxence44@gmail.com>
2023-02-08 10:24:38 -0800

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ define BaseGeometry """
+
+    def area(self):
+        """ raises exception """
+        raise Exception("area() is not implemented")
