dad1f6b60412fa6f27c199fc7aa91734ddcc9f8b
mxnctblt <thibaultmaxence44@gmail.com>
2023-02-09 06:20:05 -0800

    task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" task 0 """
+
+
+def read_file(filename=""):
+    """ function that reads a text file (UTF8) and prints it to stdout """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
