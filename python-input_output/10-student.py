6fb33ee0d7bf73cefec0669f73d7f99f90484365
mxnctblt <thibaultmaxence44@gmail.com>
2023-02-10 08:22:13 -0800

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" students """
+
+
+class Student:
+    """ defines a student """
+    def __init__(self, first_name, last_name, age):
+        """ initializes the student's information
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance
+
+        If attrs is a list of strings, only attribute names contained
+        in this list must be retrieved.
+
+        Otherwise, all attributes must be retrieved
+        """
+        if (type(attrs) == list and all(type(el) == str for el in attrs)):
+            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+        return self.__dict__
