6a21fe13e77c26611abc9dd68e394db25fbbf19c
mxnctblt <thibaultmaxence44@gmail.com>
2023-02-13 01:43:31 -0800

    task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Task 1 - create a Base class """
+
+
+class Base:
+    """ defines Base
+
+    Attributes:
+        __nb_objects (int): the number of objects
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initializes the base
+
+        Args:
+            self: represents the instance of the class
+            id (int): public instance attribute
+        """
+        if id == None:
+            Base.__nb_objects += 1
+            id = Base.__nb_objects
+        self.id = id
