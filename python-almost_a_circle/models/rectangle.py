d6f02dcf055d4c7839c5b0de0bbd2b4ec9276c44
mxnctblt <thibaultmaxence44@gmail.com>
2023-02-14 00:24:32 -0800

    task 6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,9 @@ class Rectangle(Base):
             else:
                 rec += (('#' * self.__width) + '\n')
         print(rec)
+
+    def __str__(self):
+        """ returns [Rectangle] (<id>) <x>/<y> - <width>/<height> """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                                                       self.__y, self.__width,
+                                                       self.__height)
