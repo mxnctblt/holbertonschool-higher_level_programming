50528c88f390e9fbea569edba09fe53ee02561f1
mxnctblt <thibaultmaxence44@gmail.com>
2023-03-15 06:01:02 -0700

    fix documentation

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # Task 0
+""" define list_states function """
 import MySQLdb
 import sys
 
@@ -14,14 +15,18 @@ def list_states():
     # connecting to a MySQL database
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
+
     # getting a Cursor in MySQL Python
     cur = db.cursor()
+
     # executing MySQL Queries in Python
     cur.execute("SELECT * FROM states ORDER BY id ASC")
+
     # obtaining Query Results
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
     # clean Up
     cur.close()
     db.close()
