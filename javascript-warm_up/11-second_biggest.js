d699e67110bc2561263e72686d649339b4bf01a9
mxnctblt <thibaultmaxence44@gmail.com>
2023-04-11 15:25:42 -0700

    task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.slice(2)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
+}
