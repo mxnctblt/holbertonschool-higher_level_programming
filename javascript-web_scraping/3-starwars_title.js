69473e9e183e77a5c7a4b5fe6d3d436419d577d5
mxnctblt <thibaultmaxence44@gmail.com>
2023-04-17 08:46:01 -0700

    3. Star wars movie title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const url = ('https://swapi-api.hbtn.io/api/films/' + process.argv[2])
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  }
+    console.log(JSON.parse(body).title)
+});
