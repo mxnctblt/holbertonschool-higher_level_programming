8edb9142063f04793cdfc401cac81ce16e62f123
mxnctblt <thibaultmaxence44@gmail.com>
2023-04-17 08:49:46 -0700

    semistandard --fix

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const url = ('https://swapi-api.hbtn.io/api/films/' + process.argv[2])
+const url = ('https://swapi-api.hbtn.io/api/films/' + process.argv[2]);
 const request = require('request');
 request(url, function (error, response, body) {
   if (error) {
     console.error('error:', error);
   }
-    console.log(JSON.parse(body).title)
+  console.log(JSON.parse(body).title);
 });
