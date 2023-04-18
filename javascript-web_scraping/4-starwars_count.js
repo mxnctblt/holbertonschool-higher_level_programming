f64ae8240c83a303e3e26246a5920d001b33a51e
mxnctblt <thibaultmaxence44@gmail.com>
2023-04-18 06:03:06 -0700

    semistandard --fix

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,14 +4,14 @@ request(process.argv[2], function (error, response, body) {
   if (error) {
     console.error('error:', error);
   } else {
-    results = JSON.parse(body).results;
+    const results = JSON.parse(body).results;
     let count = 0;
     for (let i = 0; i < results.length; ++i) {
       const characters = results[i].characters;
       for (let j = 0; j < characters.length; ++j) {
-	if (characters[j].endsWith('/18/') === true) {
-	count += 1;
-	}
+        if (characters[j].endsWith('/18/') === true) {
+          count += 1;
+        }
       }
     }
     console.log(count);
